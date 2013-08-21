f86b43066c304d2fe3371940904beeba8ce1498e
Roman Scherer <roman.scherer@burningswell.com>
2013-08-21 22:51:00 +0200

    No eval.

--- src/request/core.clj
+++ src/request/core.clj
@@ -3,18 +3,18 @@
             [request.util :as util]))
 
 (defmacro defroutes [name routes & [opts]]
-  (let [routes# routes
-        opts# (eval opts)]
-    `(do (def ~name
-           ~(zipmap (map :route-name routes#)
-                    (map (partial merge opts#) routes#)))
-         (def ~'path-for (request.util/path-for-routes ~name))
-         (def ~'url-for (request.util/url-for-routes ~name))
-         (defn ~'body [& args#]
-           (apply request.platform/body ~name args#))
-         (defn ~'body<! [& args#]
-           (apply request.platform/body<! ~name args#))
-         (defn ~'http [& args#]
-           (apply request.platform/http ~name args#))
-         (defn ~'http<! [& args#]
-           (apply request.platform/http<! ~name args#)))))
+  `(do (def ~name
+         (let [routes# ~routes
+               opts# ~opts]
+           (zipmap (map :route-name routes#)
+                   (map (partial merge opts#) routes#))))
+       (def ~'path-for (request.util/path-for-routes ~name))
+       (def ~'url-for (request.util/url-for-routes ~name))
+       (defn ~'body [& args#]
+         (apply request.platform/body ~name args#))
+       (defn ~'body<! [& args#]
+         (apply request.platform/body<! ~name args#))
+       (defn ~'http [& args#]
+         (apply request.platform/http ~name args#))
+       (defn ~'http<! [& args#]
+         (apply request.platform/http<! ~name args#))))
