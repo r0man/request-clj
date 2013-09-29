395f397d71168e2ab3928b401ac4048b16789560
Roman Scherer <roman.scherer@burningswell.com>
2013-09-29 19:06:03 +0200

    lein-release plugin: preparing 0.1.4 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject request-clj "0.1.4-SNAPSHOT"
+(defproject request-clj "0.1.4"
   :description "A HTTP library for Clojure & ClojureScript."
   :url "http://github.com/r0man/request-clj"
   :license {:name "Eclipse Public License"
