db5fad46e16c167f5eef26ed6312ed8f3d42902a
Roman Scherer <roman.scherer@burningswell.com>
2014-01-19 13:54:37 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -6,32 +6,33 @@
   :min-lein-version "2.0.0"
   :lein-release {:deploy-via :clojars}
   :dependencies [[clj-http "0.7.8"]
-                 [cljs-http "0.1.2"]
+                 [cljs-http "0.1.3"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2080"]]
+  :cljsbuild {:builds []}
   :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.3.2"]]
-                   :plugins [[com.cemerick/austin "0.1.3"]
-                             [com.cemerick/clojurescript.test "0.2.1"]]
+                   :plugins [[com.keminglabs/cljx "0.3.2"] ;; Must be before Austin: https://github.com/cemerick/austin/issues/37
+                             [com.cemerick/austin "0.1.3"]
+                             [com.cemerick/clojurescript.test "0.2.1"]
+                             [lein-cljsbuild "1.0.1"]]
+                   :hooks [cljx.hooks leiningen.cljsbuild]
+                   :cljx {:builds [{:source-paths ["src"]
+                                    :output-path "target/classes"
+                                    :rules :clj}
+                                   {:source-paths ["src"]
+                                    :output-path "target/classes"
+                                    :rules :cljs}
+                                   {:source-paths ["test"]
+                                    :output-path "target/test-classes"
+                                    :rules :clj}
+                                   {:source-paths ["test"]
+                                    :output-path "target/test-classes"
+                                    :rules :cljs}]}
+                   :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
+                               :builds [{:source-paths ["target/classes" "target/test-classes"]
+                                         :compiler {:output-to "target/testable.js"
+                                                    :optimizations :advanced
+                                                    :pretty-print true}}]}
+                   :test-paths ["target/test-classes"]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}}}
-  :plugins [[com.keminglabs/cljx "0.3.2"]
-            [lein-cljsbuild "1.0.1"]]
-  :hooks [cljx.hooks leiningen.cljsbuild]
-  :cljx {:builds [{:source-paths ["src"]
-                   :output-path "target/classes"
-                   :rules :clj}
-                  {:source-paths ["src"]
-                   :output-path "target/classes"
-                   :rules :cljs}
-                  {:source-paths ["test"]
-                   :output-path "target/test-classes"
-                   :rules :clj}
-                  {:source-paths ["test"]
-                   :output-path "target/test-classes"
-                   :rules :cljs}]}
-  :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
-              :builds [{:source-paths ["target/classes" "target/test-classes"]
-                        :compiler {:output-to "target/testable.js"
-                                   :optimizations :advanced
-                                   :pretty-print true}}]}
-  :test-paths ["target/test-classes"]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
