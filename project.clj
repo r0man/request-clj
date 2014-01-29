4119a6c40245b52d383b61b02cf876814da2ee14
Roman Scherer <roman.scherer@burningswell.com>
2014-01-29 22:35:44 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :min-lein-version "2.0.0"
   :lein-release {:deploy-via :clojars}
   :dependencies [[clj-http "0.7.8"]
-                 [cljs-http "0.1.3"]
+                 [cljs-http "0.1.5"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2080"]]
   :cljsbuild {:builds []}
   :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.3.2"]]
                    :plugins [[com.keminglabs/cljx "0.3.2"] ;; Must be before Austin: https://github.com/cemerick/austin/issues/37
                              [com.cemerick/austin "0.1.3"]
-                             [com.cemerick/clojurescript.test "0.2.1"]
+                             [com.cemerick/clojurescript.test "0.2.2"]
                              [lein-cljsbuild "1.0.1"]]
                    :hooks [cljx.hooks leiningen.cljsbuild]
                    :cljx {:builds [{:source-paths ["src"]
