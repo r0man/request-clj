b5dbaa904a0b4665669111239315d2a48ca7abb1
Roman Scherer <roman@burningswell.com>
2014-11-23 13:25:45 +0100

    Version 0.2.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject request-clj "0.2.8-SNAPSHOT"
+(defproject request-clj "0.2.8"
   :description "A HTTP library for Clojure & ClojureScript."
   :url "https://github.com/r0man/request-clj"
   :license {:name "Eclipse Public License"
