4fff44d57bc85cdcff7222f9052b2382ac8d76e2
Roman Scherer <roman@burningswell.com>
2014-11-23 13:25:25 +0100

    Don't leak cljx

--- project.clj
+++ project.clj
@@ -6,9 +6,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[clj-http "1.0.1"]
                  [cljs-http "0.1.20"]
-                 [routes-clj "0.1.2"]
+                 [routes-clj "0.1.3"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]]
+  :aliases {"ci" ["do" ["difftest"] ["lint"]]
+            "lint" ["do"  ["eastwood"]]
+            "test-ancient" ["test"]}
   :cljx {:builds [{:source-paths ["src"]
                    :output-path "target/classes"
                    :rules :clj}
@@ -21,18 +24,21 @@
                   {:source-paths ["test"]
                    :output-path "target/test-classes"
                    :rules :cljs}]}
+  :cljsbuild {:test-commands {"node" ["node" :node-runner "target/testable.js"]
+                              "phantom" ["phantomjs" :runner "target/testable.js"]}
+              :builds [{:source-paths ["target/classes" "target/test-classes"]
+                        :compiler {:output-to "target/testable.js"
+                                   :optimizations :advanced
+                                   :pretty-print true}}]}
   :deploy-repositories [["releases" :clojars]]
   :prep-tasks [["cljx" "once"]]
   :profiles {:dev {:plugins [[com.cemerick/austin "0.1.5"]
                              [com.cemerick/clojurescript.test "0.3.1"]
-                             [com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
-                             [lein-cljsbuild "1.0.3"]]
+                             [jonase/eastwood "0.2.0"]
+                             [lein-cljsbuild "1.0.3"]
+                             [lein-difftest "2.0.0"]
+                             [org.clojars.cemerick/cljx "0.5.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
                    :hooks [leiningen.cljsbuild]
-                   :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
-                               :builds [{:source-paths ["target/classes" "target/test-classes"]
-                                         :compiler {:output-to "target/testable.js"
-                                                    :optimizations :advanced
-                                                    :pretty-print true}}]}
-                   :prep-tasks [["cljx" "once"] ["cljsbuild" "once"]]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
-                   :test-paths ["target/test-classes"]}})
+                   :test-paths ["target/test-classes"]}
+             :test {:prep-tasks [["cljsbuild" "once"]]}})
