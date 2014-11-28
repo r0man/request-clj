83408aa95e114f00ea12823179c843429b7d2dcf
Roman Scherer <roman@burningswell.com>
2014-11-28 17:39:46 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[clj-http "1.0.1"]
-                 [cljs-http "0.1.20"]
+                 [cljs-http "0.1.21"]
                  [routes-clj "0.1.3"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]]
