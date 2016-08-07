23e907b295d321e7e6a6cf009b4180113c6bf4d2
r0man <roman@burningswell.com>
2016-08-07 13:41:44 +0200

    Version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject request-clj "0.3.0-SNAPSHOT"
+(defproject request-clj "0.3.0"
   :description "A HTTP library for Clojure & ClojureScript."
   :url "https://github.com/r0man/request-clj"
   :license {:name "Eclipse Public License"
